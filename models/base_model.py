d0d0ca16f49eaa5d8b6237dea9c2f7679f751f14
KrasniKot <6187@holbertonstudents.com>
2023-07-05 14:52:28 -0700

    to_dict now using a for loop

--- models/base_model.py
+++ models/base_model.py
@@ -8,30 +8,31 @@ from datetime import datetime
 
 
 class BaseModel():
-    """
-    Defines a BaseModel
-    """
+    """Define all attributes"""
 
     def __init__(self):
         """Constructor method"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.update_at = datetime.utcnow()
+        self.created_at = datetime.now()
+        self.update_at = datetime.now()
 
     def __str__(self):
         """Prints clase, id and dict"""
-        name = type(self).__name
-        return "[{}] ({}) {}".format(name, self.id, self.__dict__)
+        return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         """Updates date"""
-        self.update_at = datetime.utcnow()
+        self.update_at = datetime.now()
 
     def to_dict(self):
         """Returns object dictionary representation"""
-        dict_cpy = self.__dict__.copy()
-        dict_cpy["__class__"] = type(self).__name__
-        dict_cpy["created_at"] = self.created_at.isoformat()
-        dict_cpy["update_at"] = self.update_at.isoformat()
-        return (dict_cpy)
+        dict_cpy = {}
 
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "update_at":
+                dict_cpy[key] = value.isoformat()
+            else:
+                dict_cpy[key] = value
+        dict_cpy["__class__"] = type(self).__name__
+
+        return (dict_cpy)
