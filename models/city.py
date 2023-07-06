1add559d92cf44bd816087a1d10e0c836e8f97c1
micaelapicco <micaelaabril421@gmail.com>
2023-07-06 16:43:52 -0700

    add city, reviw, state, amenity and place

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Class City that inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Class City"""
+    name = ""
+    state_id = ""
---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Class States that inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class State"""
+    name = ""
