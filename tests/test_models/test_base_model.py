c2d73dc7d25cfec75f247b36436dc546a445e8a2
micaelapicco <micaelaabril421@gmail.com>
2023-07-07 15:50:00 -0700

    test updated

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@
 Unittest for class BaseModel methods
 """
 
+from datetime import datetime
 import unittest
 from models.base_model import BaseModel
 
@@ -10,10 +11,25 @@ from models.base_model import BaseModel
 class Test_base_model(unittest.TestCase):
     """Test of Class BaseModel"""
 
+    def test_attributes_type(self):
+        """checks the types of attributes"""
+        test = BaseModel()
+        self.assertEqual(type(test.created_at), datetime)
+        self.assertEqual(type(test.updated_at), datetime)
+        self.assertEqual(type(test.id), str)
+
+    def test_to_dict(self):
+        """Checks the type to returns to dict and if created and updated are str"""
+        instance = BaseModel()
+        test = instance.to_dict()
+        self.assertEqual(type(test), dict)
+        self.assertEqual(type(test["created_at"]), str)
+        self.assertEqual(type(test["updated_at"]), str)
+
     def test_str(self):
         """Checks if name is an empty string"""
         test = BaseModel()
-        compare = f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        compare = f"[{test.__class__.__name__}] ({test.id}) {test.__dict__}"
         self.assertEqual(str(test), compare)
 
 
