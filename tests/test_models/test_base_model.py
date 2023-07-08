36dce54ec4852513429ce6443184309855242092
KrasniKot <6187@holbertonstudents.com>
2023-07-07 21:36:27 -0700

    added more tests for basemodel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,12 +14,19 @@ class Test_base_model(unittest.TestCase):
     def test_attributes_type(self):
         """checks the types of attributes"""
         test = BaseModel()
+        self.assertTrue(hasattr(test, "id"))
+        self.assertTrue(hasattr(test, "created_at"))
+        self.assertTrue(hasattr(test, "updated_at"))
+
         self.assertEqual(type(test.created_at), datetime)
         self.assertEqual(type(test.updated_at), datetime)
         self.assertEqual(type(test.id), str)
 
     def test_to_dict(self):
-        """Checks the type to returns to dict and if created and updated are str"""
+        """
+        Checks the type to returns to dict
+        and if created and updated are str
+        """
         instance = BaseModel()
         test = instance.to_dict()
         self.assertEqual(type(test), dict)
@@ -31,6 +38,43 @@ class Test_base_model(unittest.TestCase):
         test = BaseModel()
         compare = f"[{test.__class__.__name__}] ({test.id}) {test.__dict__}"
         self.assertEqual(str(test), compare)
+
+    def test_save(self):
+        """Tests the function save"""
+        test = BaseModel()
+        test.save()
+        self.assertEqual(str(test.updated_at)[:20], str(datetime.now())[:20])
+
+    def test_setattr(self):
+        """Tests for manually setted attributes"""
+        test = BaseModel
+
+        test.string = "a string"
+        test.integer = 1
+        test.float_num = 1.1
+        test.float_inf = float("inf")
+        test.flan = float("nan")
+        test.empty_list = []
+        test.dictionary = {}
+        test.boo = True
+
+        self.assertTrue(hasattr(test, "string"))
+        self.assertTrue(hasattr(test, "integer"))
+        self.assertTrue(hasattr(test, "float_num"))
+        self.assertTrue(hasattr(test, "float_inf"))
+        self.assertTrue(hasattr(test, "flan"))
+        self.assertTrue(hasattr(test, "empty_list"))
+        self.assertTrue(hasattr(test, "dictionary"))
+        self.assertTrue(hasattr(test, "boo"))
+
+        self.assertTrue(test.string == "a string")
+        self.assertTrue(test.integer == 1)
+        self.assertTrue(test.float_num == 1.1)
+        self.assertTrue(test.float_inf == float("inf"))
+        self.assertTrue(test.flan != float("nan"))
+        self.assertTrue(test.empty_list == [])
+        self.assertTrue(test.dictionary == {})
+        self.assertTrue(test.boo is True)
 
 
 if __name__ == '__main__':
