570788eccf2c22202873bdd928936107b6203cff
KrasniKot <6187@holbertonstudents.com>
2023-07-07 22:42:20 -0700

    more tests added for basemodel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -52,7 +52,7 @@ class Test_base_model(unittest.TestCase):
         test.string = "a string"
         test.integer = 1
         test.float_num = 1.1
-        test.float_inf = float("inf")
+        test.float2 = float("inf")
         test.flan = float("nan")
         test.empty_list = []
         test.dictionary = {}
@@ -61,20 +61,47 @@ class Test_base_model(unittest.TestCase):
         self.assertTrue(hasattr(test, "string"))
         self.assertTrue(hasattr(test, "integer"))
         self.assertTrue(hasattr(test, "float_num"))
-        self.assertTrue(hasattr(test, "float_inf"))
+        self.assertTrue(hasattr(test, "float2"))
         self.assertTrue(hasattr(test, "flan"))
         self.assertTrue(hasattr(test, "empty_list"))
         self.assertTrue(hasattr(test, "dictionary"))
         self.assertTrue(hasattr(test, "boo"))
 
-        self.assertTrue(test.string == "a string")
-        self.assertTrue(test.integer == 1)
-        self.assertTrue(test.float_num == 1.1)
-        self.assertTrue(test.float_inf == float("inf"))
-        self.assertTrue(test.flan != float("nan"))
-        self.assertTrue(test.empty_list == [])
-        self.assertTrue(test.dictionary == {})
-        self.assertTrue(test.boo is True)
+        self.assertTrue(
+                test.string == "a string" and type(test.string) == str)
+        self.assertTrue(
+                test.integer == 1 and type(test.integer) == int)
+        self.assertTrue(
+                test.float_num == 1.1 and type(test.float_num == float))
+        self.assertTrue(
+                test.float2 == float("inf") and type(test.float2) == float)
+        self.assertTrue(
+                test.flan != float("nan") and type(test.flan) == float)
+        self.assertTrue(
+                test.empty_list == [] and type(test.empty_list) == list)
+        self.assertTrue(
+                test.dictionary == {} and type(test.dictionary) == dict)
+        self.assertTrue(test.boo is True and type(test.boo) == bool)
+
+    def test_initialize_attr(self):
+        """Test for manually initialized attributes"""
+        at = datetime.now().isoformat()
+        test = BaseModel(
+                id="a random id",
+                created_at=str(at),
+                updated_at=str(at))
+
+        self.assertTrue(hasattr(test, "id"))
+        self.assertTrue(hasattr(test, "created_at"))
+        self.assertTrue(hasattr(test, "updated_at"))
+
+        self.assertEqual(test.id, "a random id")
+        self.assertEqual(test.created_at.isoformat(), at)
+        self.assertEqual(test.created_at.isoformat(), at)
+
+        self.assertEqual(type(test.created_at), datetime)
+        self.assertEqual(type(test.updated_at), datetime)
+        self.assertEqual(type(test.id), str)
 
 
 if __name__ == '__main__':
